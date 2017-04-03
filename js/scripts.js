e82b8099dc7591ac0dd33ff351f04a2b3d78c235
Elan Baharie <0elan1@gmail.com>
2017-04-03 16:40:07 -0700

    make sentence an object

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,39 @@
-//U.I.
+//B.L.
+var sentObj = function(sentOne, sentTwo, stringLength, cols, rows) {
+  this.origSent = sentOne;
+  this.finalSent = sentTwo;
+  this.sentLength = stringLength;
+  this.numOfCol = cols;
+  this.numOfRows = rows;
+}
 
 var formatSent = function(rawSent) {
-
   var finalString = rawSent.replace(/[\W+\d+]/g, "");
   return finalString;
-
 };
 
+var numOfChar = function(letters) {
+  return letters.length;
+}
 
-//B.L.
+
+//U.I.
 
 $(document).ready(function() {
   $("#trns-form").submit(function(event) {
     event.preventDefault();
-    var formSent = $("#userSent").val();
-    var ciphRes = formatSent(formSent);
 
-    $("#response").val(ciphRes);
+    var newsentObj = new sentObj();
+
+    newsentObj.origSent = $("#userSent").val();
+
+    newsentObj.finalSent = formatSent(newsentObj.origSent);
+
+    newsentObj.sentLength = numOfChar(newsentObj.finalSent);
+
+
+
+    $("#response").val(count);
   });
 
 });// Doc Ready
